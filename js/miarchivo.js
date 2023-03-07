b7619570ec43543b9f223a1cbbf239d7e051ba20
Julieta <julietaagosanchez@gmail.com>
2023-03-07 19:41:12 -0300

    agrege flujo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,13 +1,13 @@
 class cliente {
-    constructor (num_cliente, nombre, apellido,  DNI, fecha, minconsumido, cp, prec) {
+    constructor (num_cliente, nombre, apellido,  DNI, fecha, minconsumido, comboplus, preciocombo) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.DNI = DNI;
     this.num_cliente = num_cliente;
     this.fecha = fecha;
     this.minconsumido = minconsumido;
-    this.comboplus=cp;
-    this.preciocombo=prec;
+    this.comboplus=comboplus;
+    this.preciocombo=preciocombo;
 }
 }
 
@@ -15,17 +15,42 @@ class empresa{
     constructor(precio, minutos,){
         this.precio=precio;
         this.minutos=minutos;
+        this.listaclientes= [];
     }
     Agregar_cliente(cliente){
-    const listaclientes= []
-    listaclientes.push(cliente);
+    this.listaclientes.push(cliente);
     }
     ImprimirCliente(cliente){
         document.write(cliente.nombre, cliente.apellido, cliente.DNI, cliente.fecha, cliente.minconsumido );
     }    
+
+    Imprimir_ListaCliente(){
+        for(cliente of this.listaclientes)
+         document.write(cliente.nombre + " "  + cliente.apellido + " "  + cliente.DNI + " "  +
+         cliente.fecha + " "  + cliente.minconsumido + " "  + cliente.comboplus + " " + cliente.preciocombo);
+
+    }
 }
 
 const telecentro  =  new empresa("$4000", 900);
 const cliente_1 = new cliente (0, "Juan", "Perez", 44728523, "15/03/2003", 800, true, telecentro.precio);
-telecentro.Agregar_cliente(cliente_1);
-telecentro.ImprimirCliente(cliente_1);
+
+// telecentro.Agregar_cliente(cliente_1);
+
+
+let contador=0
+let nombres = prompt("ingrese el nombre del cliente o 0 para finalizar")
+
+while (nombres !== "0" ){
+    let ape = prompt("ingrese el apellido del cliente")
+    let doc = prompt("ingrese el dni del cliente")
+    let date = prompt("ingrese el fecha de alta del cliente")
+    let mincons = prompt("ingrese minutos consumidos del cliente")
+    let combo_plus = prompt("ingrese si tiene combo plus del cliente")
+    contador +=1; 
+    const cliente_2= new cliente (contador, nombres,ape,doc,date,mincons,combo_plus, telecentro.precio)
+    telecentro.Agregar_cliente(cliente_2);
+    nombres = prompt("ingrese el nombre del cliente o 0 para finalizar")
+}
+
+telecentro.Imprimir_ListaCliente();
