1f267fe2fd4a28322f0fc9bd84be9c9dc5ccc4c5
Julieta <julietaagosanchez@gmail.com>
2023-03-14 01:44:42 -0300

    javascript a proyecto de gimnasio

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,57 +1,31 @@
-class cliente {
-    constructor (num_cliente, nombre, apellido,  DNI, fecha, minconsumido, comboplus, preciocombo) {
-    this.nombre = nombre;
+class Socio {
+    constructor (nombre, apellido, fechadeNac,edad) {
+    this.nombre=nombre;
     this.apellido = apellido;
-    this.DNI = DNI;
-    this.num_cliente = num_cliente;
-    this.fecha = fecha;
-    this.minconsumido = minconsumido;
-    this.comboplus=comboplus;
-    this.preciocombo=preciocombo;
-}
-}
-
-class empresa{
-    constructor(precio, minutos,){
-        this.precio=precio;
-        this.minutos=minutos;
-        this.listaclientes= [];
+    this.fechadeNac = Date.parse(fechadeNac).toString();
+    this.edad = edad;
     }
-    Agregar_cliente(cliente){
-    this.listaclientes.push(cliente);
+    Imprimir(persona){
+        console.log(persona.nombre + " " + persona.apellido + " "+ persona.fechadeNac + " "+ persona.edad )
     }
-    ImprimirCliente(cliente){
-        document.write(cliente.nombre, cliente.apellido, cliente.DNI, cliente.fecha, cliente.minconsumido );
-    }    
-
-    Imprimir_ListaCliente(){
-        for(cliente of this.listaclientes)
-
-         document.write( "\n ***************" + cliente.nombre + " "  + cliente.apellido + " "  + cliente.DNI + " "  +
-         cliente.fecha + " "  + cliente.minconsumido + " "  + cliente.comboplus + " " + cliente.preciocombo);
-        
-    }
-}
-
-const telecentro  =  new empresa("$4000", 900);
-const cliente_1 = new cliente (0, "Juan", "Perez", 44728523, "15/03/2003", 800, true, telecentro.precio);
-
-// telecentro.Agregar_cliente(cliente_1);
+ }
 
 
-let contador=0
-let nombres = prompt("ingrese el nombre del cliente o 0 para finalizar")
+const nombre = prompt("Ingrese su nombre")
 
-while (nombres !== "0" ){
-    let ape = prompt("ingrese el apellido del cliente")
-    let doc = prompt("ingrese el dni del cliente")
-    let date = prompt("ingrese el fecha de alta del cliente")
-    let mincons = prompt("ingrese minutos consumidos del cliente")
-    let combo_plus = prompt("ingrese si tiene combo plus del cliente")
-    contador +=1; 
-    const cliente_2= new cliente (contador, nombres,ape,doc,date,mincons,combo_plus, telecentro.precio)
-    telecentro.Agregar_cliente(cliente_2);
-    nombres = prompt("ingrese el nombre del cliente o 0 para finalizar")
-}
+alert(nombre + "! Te damos la bienvenida a La Estación del Fitness!")
 
-telecentro.Imprimir_ListaCliente();
+const apellido = prompt("Ingrese su apellido")
+let fechadeNac = prompt("Ingrese su fecha de nacimiento")
+let fechaNac = new Date (fechadeNac);
+const nacimiento = fechaNac.getFullYear();
+let fechaHoy = new Date ();
+const hoy= fechaHoy.getFullYear();
+
+let edad = hoy - nacimiento ; 
+console.log(edad)
+
+let persona = new Socio (nombre, apellido, fechadeNac, edad)
+
+persona.Imprimir(persona);
+
