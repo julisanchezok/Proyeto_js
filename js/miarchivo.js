81f164cb8f7d06de9be0fa9cda0fdad323fc6305
Julieta <julietaagosanchez@gmail.com>
2023-04-17 02:15:31 -0300

    agrege inscribirse a actividades

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -18,9 +18,9 @@ let mainBody= document.getElementById('body')
 
 
 const formularioLogin= document.getElementById('formularioLogin')
+let isLoggedIn = false;
 
 //Verifying user existence
-
 const login = (e) => {
     e.preventDefault();
 
@@ -30,12 +30,16 @@ const login = (e) => {
 
     if (userEncontrado){
         if(userEncontrado.contraseña === password){
-            console.log("logueado correctamente", userEncontrado)
-            registro.className += ' d-none';
-            mainBody.className+= ' d-block';
-            popUp.className += '  d-flex justify-content-start flex-column align-items-center'
-            mensajeBienvenida(userEncontrado);
-
+            localStorage.setItem('isLoggedIn', true);
+            if (localStorage.getItem('isLoggedIn')) {
+                mainBody.className += ' d-block';
+                registro.className += ' d-none';
+                mainBody.className+= ' d-block';
+                popUp.className += '  d-flex justify-content-start flex-column align-items-center'
+                mensajeBienvenida(userEncontrado);
+                isLoggedIn = true;
+                console.log("logueado correctamente", userEncontrado)
+            } 
         }
         else{
             console.log("usuario o contraseña incorrecta")
@@ -44,9 +48,14 @@ const login = (e) => {
    else{
     console.log("usuario o contraseña incorrecta")
    } 
+
 }
 
-formularioLogin.addEventListener('submit', login)
+if(formularioLogin){
+    formularioLogin.addEventListener('submit', login)
+}
+
+
 
 // Welcome message with name and gender 
 
@@ -63,8 +72,6 @@ function mensajeBienvenida(datos){
      
     let claseInscripto = document.getElementById('clasesInscripto')
     claseInscripto.innerHTML = datos.clase 
-
-
         
 }
 
@@ -74,16 +81,159 @@ function mensajeBienvenida(datos){
 let popUp= document.getElementById('popUp')
 let botonCerrarPP= document.getElementById('botonCerrarPop')
 
-function cerrarPopUp(){
-    popUp.className += ' d-none';
+if(popUp){
+    function cerrarPopUp(){
+        popUp.className += ' d-none';
+    }
 }
 
-botonCerrarPP.onclick = () => {cerrarPopUp()}
 
+if(popUp){
+    botonCerrarPP.onclick = () => {cerrarPopUp()}
+}
+
+if(popUp){
 setTimeout(() => {
     cerrarPopUp()
-}, 15000)
+}, 20000)
+}
 
-// Log out Script 
-let cerrarSesion= document.getElementById('logout')
-cerrarSesion.onclick =() => {}
+let cerrarSesion = document.getElementById('logout');
+if(cerrarSesion){
+    cerrarSesion.onclick = () => {
+        localStorage.setItem('isLoggedIn', false);
+        location.reload();
+    };
+}
+
+
+//AJAX FETCH ACTIVITIES 
+
+
+
+const promesa = new Promise((resolve, reject) => {
+
+    const actividadesContainer = document.getElementById('actividades')
+    fetch('../data/actividades.json')
+    .then(data => data.json())
+    .then(actividades => {
+        console.log(actividades)
+        actividades.forEach(actividad => {
+            actividadesContainer.innerHTML+=`
+            <div class="item" data-actividad="${actividad.nombre}">
+                <img src=${actividad.imgUrl} class="itemimg" alt="${actividad.nombre}">
+                <h4 class="text_item" > ${actividad.nombre} </h4>
+                <p> ${actividad.descripcion}</p>
+                <button id="inscripcion" class="btn btn-primary"> Inscribirme </button>
+            </div>
+            `
+        });
+        //for each button I click on, it will show up the div class
+        const res =  inscribirseboton();
+    })
+
+    resolve(
+       console.log("¡La promesa se resolvió correctamente!")
+    );
+    reject('Hubo un error al resolver la promesa');
+
+
+  });
+  
+  promesa.then((resultado) => {
+    console.log(resultado); // prints: "¡La promesa se resolvió correctamente!"
+  })
+  .catch((error) => {
+    console.error(error); // prints: "Hubo un error al resolver la promesa"
+  });
+  
+// function for on click event 
+//   const inscripcion = (e) => {
+   
+//     const containerPInsc = document.getElementById("PopUpInscripcion")
+//     containerPInsc.className += ' d-block'
+//     fetch('../data/actividades.json')
+//     .then(data => data.json())
+//     .then(actividades => {
+//         actividades.forEach(actividad => {
+
+//         containerPInsc.innerHTML += `
+        
+//         <button id="botonCerrarPop"> X </button>
+//         <p id="welcome"></p>
+//         <p> Horarios de ${actividad.nombre}: </p>
+//         <p id="clasesInscripto"> ${actividad.horario}</p>
+    
+//     `;
+//     })
+//     })
+//     }
+//js
+    // const inscripcion = (e) => {
+    //     const actividad = e.target.getAttribute('data-actividad');
+    //     const containerPInsc = document.getElementById("PopUpInscripcion");
+    //     containerPInsc.className += ' d-block'
+    //     containerPInsc.innerHTML += `
+    //         <button id="botonCerrarPop"> X </button>
+    //         <p id="welcome"></p>
+    //         <p> Horarios de ${actividad.nombre}: </p>
+    //     `;
+    //     fetch('../data/actividades.json')
+    //     .then(data => data.json())
+    //     .then(actividades => {
+    //         const horarios = actividades.find(a => a.nombre === actividad).horario;
+    //         horarios.forEach(horario => {
+    //             const p = document.createElement('p');
+    //             p.innerHTML = horario;
+    //             containerPInsc.appendChild(p);
+    //         });
+    //     });
+    // }
+
+    const inscripcion = (e) => {
+        const actividad = e.target.closest('.item').dataset.actividad;
+        const containerPInsc = document.getElementById("PopUpInscripcion");
+        containerPInsc.className = containerPInsc.className.replace('d-none', 'd-block');
+        containerPInsc.innerHTML = `
+            <button id="botonCerrarPopUpInscripcion"> X </button>
+            <p id="welcome"></p>
+            <p> Horarios de ${actividad}: </p>
+        `;
+        fetch('../data/actividades.json')
+            .then(data => data.json())
+            .then(actividades => {
+                const actividadEncontrada = actividades.find(a => a.nombre === actividad);
+                if (actividadEncontrada) {
+                    const horarios = actividadEncontrada.horario;
+                    if (horarios) {
+                        const clasesInscripto = document.createElement("p");
+                        clasesInscripto.id = "clasesInscripto";
+                        clasesInscripto.innerText = horarios;
+                        containerPInsc.appendChild(clasesInscripto);
+                    } else {
+                        containerPInsc.innerHTML += "<p>No hay horarios disponibles para esta actividad</p>";
+                    }
+                } else {
+                    containerPInsc.innerHTML += "<p>No se encontró la actividad seleccionada</p>";
+                }
+            });
+        containerPInsc.className = containerPInsc.className.replace('d-none', 'd-block');
+        document.getElementById("botonCerrarPopUpInscripcion").addEventListener("click", cerrarPopUpInscripcion);
+    }
+    
+    const cerrarPopUpInscripcion = () => {
+        const containerPInsc = document.getElementById("PopUpInscripcion");
+        containerPInsc.className = containerPInsc.className.replace('d-block', 'd-none');
+
+    }
+
+
+
+//function for each button in Actividades    
+function inscribirseboton() {
+    const botonesInscripcion = document.querySelectorAll("#actividades .item #inscripcion");
+    botonesInscripcion.forEach((boton) => {
+        boton.addEventListener("click", inscripcion);
+    });
+}
+
